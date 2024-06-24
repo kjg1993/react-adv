7be76730eab816b591a7fe01c7e062da0b225495
KevinGuzman <kguzman077@gmail.com>
2024-06-24 09:43:41 -0600

    Setting once again the control props alternative

--- src/02-components-patterns/hooks/useShoppingCart.ts
+++ src/02-components-patterns/hooks/useShoppingCart.ts
@@ -1,4 +1,4 @@
-import  { useState } from 'react'
+import { useState } from 'react'
 import { Product, ProductInCart } from '../interfaces/interfaces';
 
 
@@ -19,22 +19,39 @@ export const useShoppingCart = () => {
         count: number;
         product: Product;
     }) => {
+
+        console.log( { count})
         setShoppingCart(oldShoppingCart => {
-            const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            //const productInCart: ProductInCart = oldShoppingCart[product.id] || { ...product, count: 0 };
+            // // Check if the new count is greater than 0
+            // if (Math.max(productInCart.count + count, 0) > 0) {
+            //     productInCart.count += count; // Increment by the count value
 
-            // Check if the new count is greater than 0
-            if (Math.max(productInCart.count + count, 0) > 0) {
-                productInCart.count += count; // Increment by the count value
+            //     return {
+            //         ...oldShoppingCart,
+            //         [product.id]: productInCart
+            //     };
+            // }
 
-                return {
-                    ...oldShoppingCart,
-                    [product.id]: productInCart
-                };
+            // // Remove the product if the count is 0
+            // const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+            // return rest;
+
+
+
+            // Si la cantidad es 0, eliminamos el producto del carrito
+            if (count === 0) {
+                const { [product.id]: toDelete, ...rest } = oldShoppingCart;
+                console.log('To Delete:', toDelete); // Aquí mostramos el producto a eliminar
+                return rest; // Retornamos el carrito sin el producto eliminado
             }
 
-            // Remove the product if the count is 0
-            const { [product.id]: toDelete, ...rest } = oldShoppingCart;
-            return rest;
+            // Si la cantidad no es 0, actualizamos la cantidad del producto en el carrito
+            return {
+                ...oldShoppingCart,
+                [product.id]: { ...product, count }
+            };
+
         });
     };
 
