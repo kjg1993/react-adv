092509a1da0ebefa571a789eb2f9f28b97758780
KevinGuzman <kguzman077@gmail.com>
2024-06-27 21:27:56 -0600

    Cambios pequenos

--- src/01-lazyload/layout/LazyLayout.tsx
+++ src/01-lazyload/layout/LazyLayout.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { NavLink, Routes, Route, Navigate } from 'react-router-dom';
 import { LazyPage1, LazyPage2, LazyPage3 } from '../pages';
 
-const LazyLayout = () => {
+export const LazyLayout = () => {
   return (
     <div>
       <h1>Lazy Layout Page</h1>
